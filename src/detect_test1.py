c176e29784d48cc8e013dbbc6427175417a730ca
THESKY <sszheng0@163.com>
2022-04-06 20:50:03 +0800

    update README.md

--- src/detect_test1.py
+++ src/detect_test1.py
@@ -8,10 +8,13 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 corners = cv.goodFeaturesToTrack(gray, 25, 0.01, 10)
 corners = np.int0(corners)
 corners = np.squeeze(corners)
-sort_corners = np.sort(corners, axis=0)
+# 按照x坐标排序
+sort_corners = corners[np.argsort(corners[:, 0])]
 print(sort_corners.shape, sort_corners)
-for i in corners:
+for i in sort_corners:
     x, y = i.ravel()
-    cv.circle(img, (x, y), 3, (0, 0, 255), -1)
+    cv.circle(img, (x, y), 9, (0, 0, 255), -1)
 plt.imshow(img[:, :, ::-1])
 plt.show()
+plt.savefig('./doc/demo.png')
+print(f'第一条边和第四条边之间的距离为{sort_corners[0, 1]- sort_corners[-1, 1]} pixel')
